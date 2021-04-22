dbed904cefbf7d201d77dde4f7ace89e4fc23d1b
mibaumgartner <m.baumgartner@ymail.com>
2021-04-22 17:01:29 +0200

    losses

---
+++ nndet/losses/__init__.py
@@ -0,0 +1,3 @@
+from nndet.losses.classification import focal_loss_with_logits, FocalLossWithLogits
+from nndet.losses.regression import SmoothL1Loss, smooth_l1_loss, GIoULoss
+from nndet.losses.segmentation import SoftDiceLoss
