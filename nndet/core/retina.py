38af91c31f25f90c7c6ceccad840aa7b33849789
yakeworld <gushiedu@gmail.com>
2022-12-16 20:01:59 +0800

    Update retina.py

--- nndet/core/retina.py
+++ nndet/core/retina.py
@@ -364,7 +364,8 @@ class BaseRetinaNet(AbstractModel):
             keep_idxs = probs > self.score_thresh
             probs, idx = probs[keep_idxs], idx[keep_idxs]
 
-        anchor_idxs = torch.div(idx, self.num_foreground_classes, rounding_mode="floor")
+        #anchor_idxs = torch.div(idx, self.num_foreground_classes, rounding_mode="floor")
+        anchor_idxs = torch.floor_divide(idx, self.num_foreground_classes)
         labels = idx % self.num_foreground_classes
         boxes = boxes[anchor_idxs]
 
